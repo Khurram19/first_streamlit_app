fff265de7e2588cd88875a1f837bbd96aecfad48
Muhammad Khurram Meraj <52169319+Khurram19@users.noreply.github.com>
2023-01-13 15:24:13 +0500

    another input field

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,8 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('Thankyou for adding ', fruit_choice)
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
