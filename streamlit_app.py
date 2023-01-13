ab445e59a433d5f624f15a29acd2710aee9a2f8a
Muhammad Khurram Meraj <52169319+Khurram19@users.noreply.github.com>
2023-01-13 16:34:19 +0500

    converting snowflake to function

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,17 +39,20 @@ try:
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
-streamlit.stop()
+# streamlit.stop()
+
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list") 
+    return my_cur.fetchall()
 
 
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+if streamlit.button('Get Fruit List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+  
+  
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('Thankyou for adding: ', add_my_fruit)
                 
