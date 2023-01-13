d4dabf89aaed42a90314fc100e17466882003c5a
Muhammad Khurram Meraj <52169319+Khurram19@users.noreply.github.com>
2023-01-13 15:30:00 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,5 @@ streamlit.dataframe(my_data_rows)
 
 
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
+streamlit.write('Thankyou for adding: ', add_my_fruit)
