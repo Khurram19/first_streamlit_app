79fffba5d6fcb435f7c6dd929eaa73d20ff88136
Muhammad Khurram Meraj <52169319+Khurram19@users.noreply.github.com>
2023-01-13 15:27:09 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('Thankyou for adding ', add_my_fruit)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
